735d6fdc3ad19bdcdd2a51cfc0fdba7f8600c765
cmauer69 <cmauer@mauerenterprises.com>
2017-02-06 20:26:47 -0600

    This is my first commit for the Hello Xamarin project

---
+++ HelloXamarin/HelloXamarin/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloXamarin
+{
+    class test
+    {
+        public String testStr { get; set; }
+    }
+    class test2
+    {
+        public String testStr { get; set; }
+    }
+}
